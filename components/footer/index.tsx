2a269f825c21cbf9b676480c427dc7d570ca4c75
Donghee Kim <kimdhoe1@gmail.com>
2019-10-13 08:55:03 +0900

    refactor: eslint

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { css } from '@emotion/core'
 
-const styles = {
-  container: css`
-    background-color: #f1f3f5;
-  `,
-}
-
 const Footer: React.FunctionComponent = () => (
   <footer css={styles.container}>
     <p>Footer</p>
   </footer>
 )
+
+const styles = {
+  container: css`
+    background-color: #f1f3f5;
+  `,
+}
 
 export { Footer }
